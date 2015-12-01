e5512d97f9929d9e6d7bd9a43910f082f0fec45e
서정 <cu9997@naver.com>
2015-12-01 15:22:35 +0900

    Path 	createPath 알고리즘 또 수정 		(둘러싸인거 해결x, create 메소드로 중복된 부분 분리) 		updatePath 작성

--- ADD-ON/src/MapArtifacts/Path.java
+++ ADD-ON/src/MapArtifacts/Path.java
@@ -7,16 +7,16 @@ public class Path {
 	private static ArrayList<Position> path; // 경로저장 (방문하면 삭제하도록???)
 	private static ArrayList<Position> finds; // 탐색 지점 (방문하면 삭제하도록???)
 	private static Position start_xy; // 시작 지점
+	private ArrayList<Position> path2; // 임시
 	
 	private int[][] map; // getMap()해서 지도 저장
-	private static int[][] n_path; // 지점 방문 순서 나타내는 변수
-	private int end=1;
 	
 	public static final int EAST = 1;
 	public static final int SOUTH = 2;
 	public static final int WEST = 3;
 	public static final int NORTH = 4;
 	
+	public static final int OK = 9;
 	public static final int ERROR = 999;
 
 	public static void main(String[] args)
@@ -53,26 +53,17 @@ public class Path {
 		
 		// 지도 받아오기
 		map = p_map.getMap();
-		
-		// n_path 초기화
-		n_path=new int[map.length][map[0].length];
-		for(int i=0; i<n_path.length; i++)
-		{
-			for(int j=0; j<n_path.length; j++)
-				n_path[i][j]=0;
-		}
-		
-		int x, y;
-		int fx, fy;
-		boolean move = true;
+
+		int x, y, fx, fy;
 		
 		// 시작지점 받아오기
 		x=start_xy.getX()+1; y=start_xy.getY()+1;
 		// 시작지점 저장
 		path.add(new Position(x-1, y-1));
-		
-		// 시작지점 방문 순서 처음으로 설정
-		n_path[x][y]=end++;
+
+		map[3][4]=2;
+		map[3][5]=2;
+		map[5][4]=2;
 		
 		// 위험지역 출력
 		for(int i=1;i<6;i++)
@@ -90,94 +81,13 @@ public class Path {
 			fx=finds.get(i).getX()+1;
 			fy=finds.get(i).getY()+1;
 			
-			while(true)
-			{
-				
-				if(Math.abs(fx-x)<Math.abs(fy-y)) // find 방향 중 가보지 않은 방향으로 
-				{
-					if(fy>y && map[x][y+1]!=3 && n_path[x][y+1]==0)
-						y++;
-					else if(fy<y && map[x][y-1]!=3 && n_path[x][y-1]==0)
-						y--;
-					else if(fx>x && map[x+1][y]!=3 && n_path[x+1][y]==0)
-						x++;
-					else if(fx<x && map[x-1][y]!=3 && n_path[x-1][y]==0)
-						x--;
-					else 
-						move = false;
-				}
-				else
-				{
-					if(fx>x && map[x+1][y]!=3 && n_path[x+1][y]==0)
-						x++;
-					else if(fx<x && map[x-1][y]!=3 && n_path[x-1][y]==0)
-						x--;
-					else if(fy>y && map[x][y+1]!=3 && n_path[x][y+1]==0)
-						y++;
-					else if(fy<y && map[x][y-1]!=3 && n_path[x][y-1]==0)
-						y--;
-					else 
-						move = false;
-				}
-				
-				if(!move) // find 반대 방향 중 가보지 않은 방향으로 
-				{
-					move = true;
-					
-					if(fx>=x && map[x-1][y]!=3 && n_path[x-1][y]==0)
-						x--;
-					else if(fx<=x && map[x+1][y]!=3 && n_path[x+1][y]==0)
-						x++;
-					else if(fy<=y && map[x][y+1]!=3 && n_path[x][y+1]==0)
-						y++;
-					else if(fy>=y && map[x][y-1]!=3 && n_path[x][y-1]==0)
-						y--;
-					else 
-						move = false; 
-				}
-				
-				if(!move) // 가장 예전에 간 방향으로
-				{
-					move = true;
-					
-					switch(minimun(x, y)){
-						case EAST :
-							x++;
-							break;
-						case NORTH :
-							y++;
-							break;
-						case WEST :
-							x--;
-							break;
-						case SOUTH :
-							y--;
-							break;
-						case ERROR:
-							move = false;
-					}
-				}
-
-				if(!move) // 움직임에 실패한 경우
-				{
-					System.out.println("ERROR");
-					return ;
-				}
-				
-				move = true; // 초기화
-				path.add(new Position(x-1, y-1)); // 경로 저장
-				
-				// 방문 순서 저장
-				n_path[x][y]=end++;
-				
-				if(fx==x && fy==y) // 탐색지점 도착한 경우
-				{
-					x=fx;
-					y=fy;
-					break;
-				}
-			}
-				
+			if(create(x, y, fx, fy)==ERROR)
+				return ;
+			
+			for(int j=0; j<path2.size(); j++)
+				path.add(path2.get(j));
+			
+			x=fx; y=fy;
 		}
 		
 		// path 출력
@@ -187,17 +97,143 @@ public class Path {
 	
 	// 경로 재설정하는 함수 (Position start_xy, ArrayList<Position> finds)??
 	public void updatePath(){
+		// 현재위치 받아오기
+		int x=3, y=2, p=0, fx, fy;
+		boolean a = false;
 		
+		for(int i=0; i<path.size(); i++)
+		{
+			if(("("+x+","+y+")").equalsIgnoreCase(path.get(i).getPositionString()) && !a)
+			{	
+				p=i+1;
+				a=true;
+			}
+			else if(a)
+				path.remove(p);
+		}
+		
+		for(int i=0;i<finds.size();i++)  // 위험지역으로 둘러싸인 경우 아직 해결 못함...
+		{
+			// 탐색지점 받아오기
+			fx=finds.get(i).getX()+1;
+			fy=finds.get(i).getY()+1;
+			
+			if(create(x, y, fx, fy)==ERROR)
+				return ;
+			
+			for(int j=0; j<path2.size(); j++)
+				path.add(path2.get(j));
+			
+			x=fx; y=fy;
+		}
 	}
 	
 	// 경로 전달하는 함수
 	public ArrayList<Position> getPath(){
 		return path;
 	}
+	
+	// x, y 부터 fx, fy까지 경로 구하기
+	public int create(int x, int y, int fx, int fy)
+	{
+		int end=1;
+		boolean move = true;
+	
+		// n_path 초기화
+		int [][]n_path=new int[map.length][map[0].length];
+		for(int i=0; i<n_path.length; i++)
+		{
+			for(int j=0; j<n_path[0].length; j++)
+			{
+				if(map[i][j]==3)
+					n_path[i][j]=ERROR;
+				else
+					n_path[i][j]=0;
+			}
+		}
+		
+		// 초기화
+		path2 = new ArrayList<Position>();
+		
+		while(true)
+		{
+			
+			if(Math.abs(fx-x)<Math.abs(fy-y)) // find 방향 중 가보지 않은 방향으로 
+			{
+				if(fy>y && map[x][y+1]!=3 && n_path[x][y+1]==0)
+					y++;
+				else if(fy<y && map[x][y-1]!=3 && n_path[x][y-1]==0)
+					y--;
+				else if(fx>x && map[x+1][y]!=3 && n_path[x+1][y]==0)
+					x++;
+				else if(fx<x && map[x-1][y]!=3 && n_path[x-1][y]==0)
+					x--;
+				else 
+					move = false;
+			}
+			else
+			{
+				if(fx>x && map[x+1][y]!=3 && n_path[x+1][y]==0)
+					x++;
+				else if(fx<x && map[x-1][y]!=3 && n_path[x-1][y]==0)
+					x--;
+				else if(fy>y && map[x][y+1]!=3 && n_path[x][y+1]==0)
+					y++;
+				else if(fy<y && map[x][y-1]!=3 && n_path[x][y-1]==0)
+					y--;
+				else 
+					move = false;
+			}
+			
+			if(!move) // 가장 예전에 간 방향으로
+			{	
+				move=true;
+				
+				switch(oldestMove(x, y, n_path)){
+					case EAST :
+						x++;
+						break;
+					case NORTH :
+						y++;
+						break;
+					case WEST :
+						x--;
+						break;
+					case SOUTH :
+						y--;
+						break;
+					case ERROR:
+						move = false;
+						break;
+				}
+			}
 
+			if(!move) // 움직임에 실패한 경우
+			{
+				System.out.println("ERROR");
+				return ERROR;
+			}
+			
+			move = true; // 초기화
+			path2.add(new Position(x-1, y-1)); // 경로 저장
+			
+			// 방문 순서 저장
+			n_path[x][y]=end++;
+			
+			if(fx==x && fy==y) // 탐색지점 도착한 경우
+			{
+				x=fx;
+				y=fy;
+				break;
+			}
+			
+			// 다른 탐색지점을 지나가는 경우??
+		}
+		return OK;
+	}
 	
 	// 가장 예전에 간 방향 return	
-	public int minimun(int x, int y)
+	public int oldestMove(int x, int y, int[][] n_path)
 	{
 		int a, b, c, d;
 		
@@ -205,15 +241,6 @@ public class Path {
 		b = n_path[x-1][y]; 
 		c = n_path[x][y+1]; 
 		d = n_path[x][y-1];
-		
-		if(map[x+1][y]==3)
-			a=999;
-		if(map[x-1][y]==3)
-			b=999;
-		if(map[x][y+1]==3)
-			c=999;
-		if(map[x][y-1]==3)
-			d=999;
 		
 		if(a<=b && a<=c && a<=d)
 			return EAST;
@@ -226,6 +253,5 @@ public class Path {
 		else
 			return ERROR;
 	}
-
 }
 
