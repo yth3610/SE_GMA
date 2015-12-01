12bcc30ff57b3714048908842471868d1c04e7b9
서정 <cu9997@naver.com>
2015-12-01 15:44:09 +0900

    Path 일부 메소드 권한 변경, PathManager 메소드 내용 추가

--- ADD-ON/src/MapArtifacts/PathManager.java
+++ ADD-ON/src/MapArtifacts/PathManager.java
@@ -1,10 +1,15 @@
 package MapArtifacts;
 
+import java.util.ArrayList;
+
 public class PathManager {
 
+	private static Path path;
+	
 	// 경로 초기화 값 전달
-	public void createPath(){
-		
+	public void createPath(Position start_xy, ArrayList<Position> finds){
+		path= new Path();
+		path.createPath();
 	}
 	
 	// 경로 출력...? (시퀸스 잘못 그린 것 같다 Planning a Path)
