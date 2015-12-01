c5f3c1a55f794d314a189708c3584d63b5c59c5e
서정 <cu9997@naver.com>
2015-12-01 15:28:55 +0900

    MapManager 작성

--- ADD-ON/src/MapArtifacts/MapManager.java
+++ ADD-ON/src/MapArtifacts/MapManager.java
@@ -1,15 +1,20 @@
 package MapArtifacts;
 
+import java.util.ArrayList;
+
 public class MapManager {
 
+	private static Map map;
+	
 	// 지도 초기화 값 전달
-	public void create(){
-		
+	public void create(int x, int y, ArrayList<Position> hazard){
+		map=new Map();
+		map.create();
 	}
 	
 	// 지도 재설정 값 전달
-	public void update(){
-		
+	public void update(String type, Position xy){
+		map.update(type, xy);
 	}
 	
 	// 지도 출력...??? (시퀸스 잘못 그린 것 같다 Handling map)
