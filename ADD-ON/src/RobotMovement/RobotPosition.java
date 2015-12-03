0eed5aa1205e73167c585bab8231071edf861af4
천상진 <csjin75@gmail.com>
2015-12-03 17:44:48 +0900

    주석추가 및 일부 수정

--- ADD-ON/src/RobotMovement/RobotPosition.java
+++ ADD-ON/src/RobotMovement/RobotPosition.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class RobotPosition implements Finals{
 	private Position currentPosition, nextPosition;
 	private ArrayList<Position> pathList;
-	private static int mcount = 1; //movement count
+	private static int pathCount = 1; //movement count
 	
 	Path path = new Path();
 	
@@ -22,41 +22,43 @@ public class RobotPosition implements Finals{
 		pathList.add(new Position(2, 2));
 		pathList.add(new Position(2, 1));
 		
-		//임시
+		//연동해서 쓸 부분
 		//RobotPositionManager로 부터 robot의 현재위치를 가져온다.
 		//currentPosition = RobotPositionManager.getPosition();
 		//pathList = path.getPath();
+		//for(int i = 0; i < pathList.size(); i++)
+		//	this.createNextPosition(pathList.get(i));
 		
 	}
 	
 	//path와 currentPositin을 비교하여 다음 위치를 만드는 method
-	public void createNextPosition() {
+	public void createNextPosition(Position path) {
 		nextPosition = new Position();
-		if(pathList.get(mcount).getX() - currentPosition.getX() == 1) {
+		if(path.getX() - currentPosition.getX() == 1) {
 			//EAST(+x축)으로 한칸 이동해야 하는 경우
 			nextPosition.setX(currentPosition.getX() + 1);
 			nextPosition.setY(currentPosition.getY());
 			nextPosition.setDirection(EAST); //EAST(+x)
 		} 
-		else if(pathList.get(mcount).getX() - currentPosition.getX() == -1) {
+		else if(path.getX() - currentPosition.getX() == -1) {
 			//WEST(-x축)으로 한칸 이동해야 하는 경우
 			nextPosition.setX(currentPosition.getX() - 1);
 			nextPosition.setY(currentPosition.getY());
 			nextPosition.setDirection(WEST); //WEST(-x)
 		}
-		else if(pathList.get(mcount).getY() - currentPosition.getY() == 1) {
+		else if(path.getY() - currentPosition.getY() == 1) {
 			//NORTH(+y축)으로 한칸 이동해야 하는 경우
 			nextPosition.setX(currentPosition.getX());
 			nextPosition.setY(currentPosition.getY() + 1);
 			nextPosition.setDirection(NORTH); //NORTH(+y)
 		} 
-		else if(pathList.get(mcount).getY() - currentPosition.getY() == -1) {
+		else if(path.getY() - currentPosition.getY() == -1) {
 			//SOUTH(-y축)으로 한칸 이동해야 하는 경우
 			nextPosition.setX(currentPosition.getX());
 			nextPosition.setY(currentPosition.getY() - 1);
 			nextPosition.setDirection(SOUTH); //SOUTH(-y)
 		}
-		mcount++; //다음 path를 받기 위한 변수
+		RobotMovement rm = new RobotMovement(currentPosition, nextPosition);
 	}
 	
 	//임시
@@ -69,14 +71,17 @@ public class RobotPosition implements Finals{
 		return this.nextPosition;
 	}
 	
+	public static void plusCount() {
+		pathCount++;
+	}
+	
 	public static void main(String[] args) {
 
 		RobotPosition r = new RobotPosition();
-		r.createNextPosition();
+		//r.createNextPosition();
 		System.out.println(r.getNextPosition().toString());
 		r.setCurrentPosition(1, 2);
-		r.createNextPosition();
+		//r.createNextPosition();
 		System.out.println(r.getNextPosition().toString());
 	}
 }
-
