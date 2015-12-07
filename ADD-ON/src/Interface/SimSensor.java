13b5d96bada387cc0b7bf32cb006d891f33327b2
서정 <cu9997@naver.com>
2015-12-07 23:32:29 +0900

    SimSensor 범위 수정

--- ADD-ON/src/Interface/SimSensor.java
+++ ADD-ON/src/Interface/SimSensor.java
@@ -21,28 +21,28 @@ public class SimSensor implements Finals {
 		
 		switch(position.getDirection()) {
 		case EAST:
-			if(map[position.getX() + 1][position.getY()] == HIDE_HAZARD) {
+			if(map[position.getX() + 2][position.getY()+1] == HIDE_HAZARD) {
 				mm.updateMap("Hazard", new Position(position.getX() + 1, position.getY()));
 				MapForm.moveLog("Find HIDE_HAZARD (" + (position.getX()+1) + ", " + position.getY() + ")");
 				System.out.println("Find HIDE_HAZARD (" + (position.getX()+1) + ", " + position.getY() + ")");
 			}
 	        break;
 		case SOUTH:
-			if(map[position.getX()][position.getY() - 1] == HIDE_HAZARD) {
+			if(map[position.getX()+1][position.getY()] == HIDE_HAZARD) {
 				mm.updateMap("Hazard", new Position(position.getX(), position.getY() - 1));
 				MapForm.moveLog("Find HIDE_HAZARD (" + (position.getX()) + ", " + (position.getY()-1) + ")");
 				System.out.println("Find HIDE_HAZARD (" + (position.getX()) + ", " + (position.getY()-1) + ")");
 			}
 	         break;
 		case WEST:
-			if(map[position.getX() - 1][position.getY()] == HIDE_HAZARD) { 
+			if(map[position.getX()][position.getY()+1] == HIDE_HAZARD) { 
 				mm.updateMap("Hazard", new Position(position.getX() - 1, position.getY()));
 				MapForm.moveLog("Find HIDE_HAZARD (" + (position.getX()-1) + ", " + (position.getY()) + ")");
 				System.out.println("Find HIDE_HAZARD (" + (position.getX()-1) + ", " + (position.getY()) + ")");
 			}
 			break;
 		case NORTH:
-			if(map[position.getX()][position.getY() + 1] == HIDE_HAZARD) {
+			if(map[position.getX()+1][position.getY() + 2] == HIDE_HAZARD) {
 				mm.updateMap("Hazard", new Position(position.getX(), position.getY() + 1));
 				MapForm.moveLog("Find HIDE_HAZARD (" + (position.getX()) + ", " + (position.getY()+1) + ")");
 				System.out.println("Find HIDE_HAZARD (" + (position.getX()) + ", " + (position.getY()+1) + ")");
@@ -55,22 +55,22 @@ public class SimSensor implements Finals {
 		MapManager mm = new MapManager(); // mm 생성
 		//숨겨진 colorblob을 발견한 경우
 		//4방향 모두 찾아야 하기 때문에 4개의 if문 사용
-		if(map[position.getX() + 1][position.getY()] == HIDE_COLORBLOB) { //EAST(+x) 방향
+		if(map[position.getX() + 2][position.getY()+1] == HIDE_COLORBLOB) { //EAST(+x) 방향
 			mm.updateMap("Color", new Position(position.getX() + 1, position.getY()));
 			MapForm.moveLog("Find HIDE_COLORBLOB (" + (position.getX()+1) + ", " + (position.getY()) + ")");
 			System.out.println("Find HIDE_COLORBLOB (" + (position.getX()+1) + ", " + (position.getY()) + ")");
 		}
-		if(position.getY() != 0 && map[position.getX()][position.getY() - 1] == HIDE_COLORBLOB) { //SOUTH(-x) 방향
+		if(position.getY() != 0 && map[position.getX()+1][position.getY()] == HIDE_COLORBLOB) { //SOUTH(-x) 방향
 			mm.updateMap("Color", new Position(position.getX(), position.getY() - 1));
 			MapForm.moveLog("Find HIDE_COLORBLOB (" + (position.getX()) + ", " + (position.getY()-1) + ")");
 			System.out.println("Find HIDE_COLORBLOB (" + (position.getX()) + ", " + (position.getY()-1) + ")");
 		}
-		if(position.getX() != 0 && map[position.getX() - 1][position.getY()] == HIDE_COLORBLOB) { //WEST(-x) 방향
+		if(position.getX() != 0 && map[position.getX()][position.getY()+1] == HIDE_COLORBLOB) { //WEST(-x) 방향
 			mm.updateMap("Color", new Position(position.getX() - 1, position.getY()));
 			MapForm.moveLog("Find HIDE_COLORBLOB (" + (position.getX()-1) + ", " + (position.getY()) + ")");
 			System.out.println("Find HIDE_COLORBLOB (" + (position.getX()-1) + ", " + (position.getY()) + ")");
 		}
-		if(map[position.getX()][position.getY() + 1] == HIDE_COLORBLOB) { //NORTH(+y) 방향
+		if(map[position.getX()+1][position.getY() + 2] == HIDE_COLORBLOB) { //NORTH(+y) 방향
 			mm.updateMap("Color", new Position(position.getX(), position.getY() + 1));
 			MapForm.moveLog("Find HIDE_COLORBLOB (" + (position.getX()) + ", " + (position.getY()+1) + ")");
 			System.out.println("Find HIDE_COLORBLOB (" + (position.getX()) + ", " + (position.getY()+1) + ")");
